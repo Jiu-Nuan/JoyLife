2de4bca825a116da41839887b64c1d72ed1e306e
JiuNuan <1242462013@qq.com>
2024-02-20 10:54:07 +0800

    添加头部标题下标线及动作，但下标线的偏移量为设置好。将昨晚由ref引发的错误修复

--- Demo/JoyLife/src/views/Home/index.vue
+++ Demo/JoyLife/src/views/Home/index.vue
@@ -2,19 +2,21 @@
   <div class="home-container">
     <div class="home-swiper-head">
       <i class="iconfont icon-huiyuan2"></i>
-      <div class="swpier-name">
+      <div class="swpier-name" ref="moduleRef">
         <span
           v-for="(item, idx) in moduleList"
           @click="changeSwiper(idx)"
           :class="{ active: idx === moduleIdx }"
           >{{ item }}</span
         >
+        <div class="under-line" ref="underLineRef"></div>
       </div>
       <i class="iconfont icon-sousuo"></i>
     </div>
     <swiper
       @swiper="onSwiper"
       @slideChange="onSlideChange"
+      @progress="onProgress"
       :initialSlide="moduleIdx"
       class="my-swiper"
     >
@@ -39,23 +41,10 @@
 </template>
 
 <script setup>
-import { ref, reactive } from "vue";
+import { ref, reactive, onMounted } from "vue";
 //引入swiper插件样式
 import { Swiper, SwiperSlide } from "swiper/vue";
 import "swiper/css";
-
-//加入头部内容
-const moduleList = reactive(['发现','关注', '附近', '广场舞', '音乐']);
-
-//点击切换标题
-// 使用ref来创建一个响应式的变量
-const moduleIdx = ref(0);
-
-// 定义changeSwiper函数，用于切换标题
-const changeSwiper = (idx) => {
-  moduleIdx.value = idx;
-  mySwiper?.slidTo(idx);//滑动到对应标题
-};
 
 //初始化swiper
 let mySwiper = null;
@@ -63,13 +52,81 @@ const onSwiper = (swiper) => {
   mySwiper = swiper;
 };
 
+//加入头部内容
+const moduleList = reactive(["关注", "发现", "附近", "广场舞", "音乐"]);
+
+//点击切换标题
+// 使用ref来创建一个响应式的变量
+const moduleIdx = ref(1);
+
+// 确保moduleIdx和mySwiper在组件中被正确声明和初始化
+const changeSwiper = (idx) => {
+  // 验证idx是否是一个有效的索引值
+  if (idx >= 0 && idx < mySwiper.slides.length) {
+    // 如果mySwiper存在，则滑动到对应的标题位置
+    mySwiper?.slideTo(idx);
+    // 更新moduleIdx的值
+    moduleIdx.value = idx;
+  } else {
+    console.error("Invalid index provided for changing the swiper.");
+  }
+};
+
 //把头部切换与swiper切换关联
-const onSlideChange = ({activeIndex}) => {
-  console.log(mySwiper.activeIndex);
+const onSlideChange = ({ activeIndex }) => {
+  // console.log(mySwiper.activeIndex);
   moduleIdx.value = activeIndex;
 };
 
+//下标线与选中的module进行平移和缩放
+const underLineRef = ref(null);
+const moduleRef = ref(null);
 
+//计算每个关键位置时，下标线的偏移量
+const posList = []; //下标线的偏移量
+const getModuleParams = () => {
+  const modules = moduleRef.value.querySelectorAll("span");
+  //获取下标线宽度
+  const moduleWidth = moduleRef.value.getBoundingClientRect().width;
+  //遍历每个头部标题，计算其到窗口左侧的距离
+  for (let i = 0; i < modules.length; i++) {
+    let moduleRect = modules[i].getBoundingClientRect();
+    //下标线在每个标题正下方时的距离窗口左侧的距离=当前标题距左侧的距离+当前标题宽度的一半-下标线宽度的一半
+    posList.push(moduleRect.left + moduleRect.width / 2 - moduleWidth / 2);
+  }
+};
+
+// //监听滑动过程，使用progress计算
+const onProgress = ({ progress }) => {
+  if (posList.length === 0) return;
+  //滑动到最后一个
+  if (progress === 1) {
+    //当滑动到最后一个时，下标线偏移量设置为距离数组最后一个值
+    underLineRef.value.style.transform = `translateX(${
+      posList[posList.length - 1]
+    }px)`;
+    return;
+  }
+  //其他情况
+  //计算出当前progress处于哪两个标题之间
+  let nowPosIdx = progress * (moduleList.length - 1);
+  //结果向下取整
+  let nowPosIdxInt = Math.floor(nowPosIdx);
+  //计算当前progress下标线的偏移量
+  let trans =
+    posList[nowPosIdxInt + 1] -
+    posList[nowPosIdxInt] * (nowPosIdx - nowPosIdxInt) +
+    posList[nowPosIdxInt];
+
+  underLineRef.value.style.transform = `translateX(${trans}px)`;
+  //return;
+};
+
+//getModuleParams()方法需要等页面真实dom加载完毕后才可以执行
+onMounted(() => {
+  getModuleParams();
+  console.log(posList);
+});
 </script>
 
 <style lang="scss" scoped>
@@ -90,7 +147,7 @@ const onSlideChange = ({activeIndex}) => {
     right: 0;
     height: 1rem;
     background-color: #fff;
-    padding: 1rem;
+    padding: 0.8rem;
     z-index: 2;
     @include flexbc;
 
@@ -115,6 +172,18 @@ const onSlideChange = ({activeIndex}) => {
           color: rgb(51, 51, 51);
           font-weight: bold;
         }
+      }
+
+      .under-line {
+        //相对整个头部进行定位
+        position: absolute;
+        bottom: 0.5rem;
+        left: 0;
+        transform: all 0.2s linear;
+        width: 1.875rem;
+        height: 0.1875rem;
+        background-color: rgb(248, 230, 28);
+        border-radius: 0.1875rem;
       }
     }
   }
