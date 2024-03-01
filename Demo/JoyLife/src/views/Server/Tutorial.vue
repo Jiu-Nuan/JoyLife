f6c42afdf2395c6a1d8b954229679dc7e3e40a28
JiuNuan <1242462013@qq.com>
2024-03-01 15:15:17 +0800

    调整了走马灯的显示问题

--- Demo/JoyLife/src/views/Server/Tutorial.vue
+++ Demo/JoyLife/src/views/Server/Tutorial.vue
@@ -1,31 +1,36 @@
 <template>
+  <div class="tutorial-carousel">
     <el-carousel indicator-position="inside">
       <el-carousel-item v-for="item in 4" :key="item">
         <!-- <h3 text="2xl" justify="center">{{ item }}</h3> -->
         <!-- <img src="../../mock/source/img0{{item}}.jpg" alt=""> -->
       </el-carousel-item>
     </el-carousel>
-  </template>
+  </div>
+</template>
   
   <script setup>
-import { ref, reactive } from "vue"
+import { ref, reactive } from "vue";
 </script>
 
 <style lang="scss" scoped>
-  .el-carousel__item h3 {
-    display: flex;
-    color: #475669;
-    opacity: 0.75;
-    line-height: 300px;
-    margin: 0;
-  }
-  
+
+.tutorial-carousel {
+  position: relative;
+  width: 100vw;
+  height: auto;
+  top: 3rem;
+  background-color: #fefefe;
+  border-radius: 0.5rem;
+  box-shadow: 0.125rem 0.3125rem rgba(181, 181, 191, 0.1);
+  overflow: hidden;
   .el-carousel__item:nth-child(2n) {
     background-color: #99a9bf;
   }
-  
+
   .el-carousel__item:nth-child(2n + 1) {
     background-color: #d3dce6;
   }
-  </style>
+}
+</style>
   
--- Demo/JoyLife/vite.config.js
+++ Demo/JoyLife/vite.config.js
@@ -24,8 +24,8 @@ export default defineConfig({
   },
 
   server:{
-    // host: '192.168.3.12',
-    host:'192.168.69.204',
+    host: '192.168.3.12',
+    // host:'192.168.69.204',
     //port: 5173,
   },
 })
